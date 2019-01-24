e4c891c3e4b0161cac88a21cd59e076ec8e7d996
ByungjinJun <Byungjin.Jun.55@gmail.com>
2019-01-24 15:29:01 -0600

    minor fix

--- qserver/run_server.sh
+++ qserver/run_server.sh
@@ -1,1 +1,1 @@
-go run server.go -cert cert/leaf_cert.pem -key cert/leaf_cert.key -qaddr 10.95.137.54:443 -taddr 10.95.137.54:10000
+go run server.go -cert cert/leaf_cert.pem -key cert/leaf_cert.key -qaddr 165.124.183.118:4431 -taddr 165.124.183.118:10000
